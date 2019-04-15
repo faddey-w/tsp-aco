b9ad96b16f9431b2e85a6a5ef997de680afdf983
Yaroslav Protsenko <yprotsenko@4tifier.com>
2019-04-16 00:21:50 +0300

    WIP: refactored, improving the algorithm...

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tsp_aco
 
 
 def readfile(path, country=None):
-    with open(path, encoding='ISO-8859-1') as f:
+    with open(path, encoding="ISO-8859-1") as f:
         if country is None:
             iterlines = f
         else:
@@ -18,18 +18,22 @@ def readfile(path, country=None):
     return heading, rows
 
 
-def main():
+def main(argv=None):
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file', '-F', default='worldcitiespop-part1.txt')
-    parser.add_argument('--country', '-C', default='hu')
-    parser.add_argument('-n', type=int, default=6)
-    parser.add_argument('-m', type=int, default=2)
-    parser.add_argument('--iterations', '-I', type=int)
-    parser.add_argument('--out', '-O')
-    parser.add_argument('--format', '-f', choices=['readable', 'csv', 'numpy'], default='csv')
-    parser.add_argument('--show', '-S', action='store_true')
-    opts = parser.parse_args()
+    parser.add_argument("--file", "-F", default="worldcitiespop-part1.txt")
+    parser.add_argument("--country", "-C", default="hu")
+    parser.add_argument("-n", type=int, default=6)
+    parser.add_argument("-m", type=int, default=2)
+    parser.add_argument("--iterations", "-I", type=int)
+    parser.add_argument("--n-probas", "-p", type=int)
+    parser.add_argument("--features", default='')
+    parser.add_argument("--out", "-O")
+    parser.add_argument(
+        "--format", "-f", choices=["readable", "csv", "numpy"], default="csv"
+    )
+    parser.add_argument("--show", "-S", action="store_true")
+    opts = parser.parse_args(argv)
 
     heading, rows = readfile(opts.file, opts.country)
     # heading = Country, City, AccentCity, Region, Population, Lat, Lon
@@ -37,68 +41,70 @@ def main():
     populations = [int(r[4] or 0) for r in rows]
     top_pop_idx = sorted(range(len(rows)), key=populations.__getitem__, reverse=True)
 
-    cities = [rows[i] for i in top_pop_idx[:opts.n]]
+    cities = [rows[i] for i in top_pop_idx[: opts.n]]
     lat = [float(r[5]) for r in cities]
     lon = [float(r[6]) for r in cities]
     coordinates = list(zip(lat, lon))
 
     distmat = [
         [
-            ((lat[i1]-lat[i2])**2 + (lon[i1]-lon[i2])**2) ** 0.5
+            ((lat[i1] - lat[i2]) ** 2 + (lon[i1] - lon[i2]) ** 2) ** 0.5
             for i2 in range(len(cities))
         ]
         for i1 in range(len(cities))
     ]
 
-    if opts.out == '-':
+    if opts.out == "-":
         out = sys.stdout
     elif opts.out:
-        out = open(opts.out, 'w')
+        out = open(opts.out, "w")
     else:
         out = None
     if out is not None:
         names = [r[2] for r in cities]
 
-        printers = {
-            'readable': print_readable,
-            'csv': print_csv,
-            'numpy': print_numpy,
-        }
+        printers = {"readable": print_readable, "csv": print_csv, "numpy": print_numpy}
         printers[opts.format](out, names, distmat)
 
-        if opts.out != '-':
+        if opts.out != "-":
             out.close()
 
+    heuristic = tsp_aco.Heuristic(
+        distances=distmat,
+        n_salesmans=opts.m,
+        herdness=0.5,
+        greedness=0.5,
+        evaporation=0.01,
+        n_probas=opts.n_probas or int(opts.n * math.log(opts.n)) * opts.m,
+        features=opts.features.split(','),
+    )
     tsp_aco.solve_tsp(
         distances=distmat,
-        callback=tsp_aco.GenerateVisualSvg('frames', coordinates),
-        herdness=2.0,
-        greedness=1.0,
-        evaporation=0.1,
+        callback=tsp_aco.GenerateVisualSvg("frames", coordinates),
         max_iteration=opts.iterations or int(opts.n * math.log(opts.n)),
-        n_probas=int(opts.n * math.log(opts.n)) * opts.m,
         n_salesmans=opts.m,
-        objective='max',
+        objective="max",
+        heuristic=heuristic,
     )
 
     if opts.show:
-        url = pathlib.Path(__file__).absolute().parent.joinpath('visualizer.html').as_uri()
+        url = (
+            pathlib.Path(__file__)
+            .absolute()
+            .parent.joinpath("visualizer.html")
+            .as_uri()
+        )
         webbrowser.open(url)
 
 
 def print_readable(out, names, distmap):
-    printmat = [
-        ['', *names]
-    ]
+    printmat = [["", *names]]
     for name, row in zip(names, distmap):
-        printmat.append([
-            name,
-            *('{:.2f}'.format(d) for d in row)
-        ])
+        printmat.append([name, *("{:.2f}".format(d) for d in row)])
     colw = max(map(len, (x for r in printmat for x in r)))
     for row in printmat:
-        out.write(' '.join(r.ljust(colw) for r in row))
-        out.write('\n')
+        out.write(" ".join(r.ljust(colw) for r in row))
+        out.write("\n")
 
 
 def print_csv(out, names, distmap):
@@ -110,8 +116,10 @@ def print_csv(out, names, distmap):
 
 def print_numpy(out, names, distmap):
     import numpy
+
     numpy.savetxt(out, distmap)
 
 
-if __name__ == '__main__':
+# main('-F test.csv -n 9 -m 3 -p 1 -I 100'.split())
+if __name__ == "__main__":
     main()
--- tsp_aco.py
+++ tsp_aco.py
@@ -4,197 +4,229 @@ import itertools
 import math
 
 
-def solve_tsp(distances, callback=lambda utility, path: None,
-              n_probas=None,
-              herdness=1,
-              greedness=1,
-              evaporation=0.1,
-              max_iteration=10,
-              n_salesmans=1,
-              objective='max',
-              _debug=False):
-    if _debug > 0:
-        _debug -= 1
-    n = len(distances)
-    n_probas = n_probas or n
-    assert n_salesmans < n, "if there are no less salesmans than cities, " \
-                            "you do not need to force them to travel"
-    assert objective in ('max', 'total')
+JUMP_PHEROMONE = "naive"
+# JUMP_PHEROMONE = 'heuristical'
+# JUMP_PHEROMONE = 'nondirected'
+USE_JUMP_PHEROMONE_DOWNSCALE = False
+DECOUPLE_LONGEST_BY_JUMP = False
+DECOUPLE_LONGEST_BY_MOVE = False
 
-    mean_d = sum(map(sum, distances)) / (n*n)
-    min_d = min(min(filter(bool, row)) for row in distances)
 
-    # Walk properties are responsible for building optimal paths between nodes.
-    # It is from classic ACO algorithm
+class Heuristic:
+    def __init__(
+        self,
+        distances,
+        n_salesmans,
+        greedness=1,
+        herdness=1,
+        evaporation=0.1,
+        n_probas=None,
+        features=(),
+    ):
+        self.distances = distances
+        self.n_salesmans = n_salesmans
+        self.n = n = len(distances)
+        self.n_probas = n_probas or n
+        self.herdness = herdness
+        self.evaporation = evaporation
+        self.use_jump_balancing = 'JUMP_BALANCING' in features
+        mean_d = sum(map(sum, distances)) / (n * n)
+        self.move_heuristic = [
+            _normalize([(1 / d) ** greedness if d else 0 for d in row])
+            for row in distances
+        ]
+        self.move_utility = [[1 / (n * mean_d)] * n for _ in range(n)]
+        self.jump_utility = [[1 / (n * mean_d)] * n for _ in range(n)]
+        self.jump_heuristic = [
+            [1 / (n - 1) for d in row] for i, row in enumerate(distances)
+        ]
+        self._reset_update_buffers()
+        self._update_evidences()
 
-    # walk utility is amount of ant's pheromone on the path between nodes
-    move_utility = [[1 / (n*mean_d)] * n for _ in range(n)]
-    jump_utility = [[1 / (n*mean_d)] * n for _ in range(n)]
-    move_heuristic = [
-        _softmax([
-            (1 / d) ** greedness if d else 0
-            for d in row
-        ])
-        for row in distances
-    ]
-    jump_heuristic = [
-        _softmax([
-            1
-            # d ** greedness
-            for d in row
-        ])
-        for row in distances
-    ]
-    if _debug:
-        print('initial move_utility =', move_utility[0][0])
-        print('initial jump_utility =', jump_utility[0][0])
+    def _reset_update_buffers(self):
+        n = self.n
+        self._move_utility_update = [[0] * n for _ in range(n)]
+        self._jump_utility_update = [[0] * n for _ in range(n)]
 
-    # evidences are unscaled probabilities of each transition on current round.
-    # initial evidence is proportional to heuristic, so don't do anything here
-    m_evidence = move_heuristic
-    j_evidence = jump_heuristic
+    def _update_evidences(self):
+        n = self.n
+        move_utility = self.move_utility
+        jump_utility = self.jump_utility
+        move_heuristic = self.move_heuristic
+        jump_heuristic = self.jump_heuristic
+        herdness = self.herdness
 
-    best_paths = _make_aco_proba(
-            n, n_salesmans,
-            m_evidence,
-            j_evidence,
-            greedy=True)
-    if _debug:
-        print('initial m_evidence')
-        print(_format2d(m_evidence))
-        print('initial j_evidence')
-        print(_format2d(j_evidence))
+        self._move_evidence = [
+            [move_heuristic[i][j] * (move_utility[i][j] ** herdness) for j in range(n)]
+            for i in range(n)
+        ]
+        self._jump_evidence = [
+            [jump_heuristic[i][j] * (jump_utility[i][j] ** herdness) for j in range(n)]
+            for i in range(n)
+        ]
 
-    callback(best_paths, move_utility, j_evidence)
+    def add_path(self, paths, cost):
+        lengths = [_path_len(p, self.distances) for p in paths]
+        max_length = max(lengths)
+        jump_utility_update = self._jump_utility_update
+        move_utility_update = self._move_utility_update
 
-    for _ in range(max_iteration):
-        # there is no early-stop criteria actually
+        # commit to pheromone update buffer
 
-        # accumulate new pheromone in separate buffer
-        # to keep transition probabilities same during one round of probas
-        move_utility_update = [[0] * n for _ in range(n)]
-        jump_utility_update = [[0] * n for _ in range(n)]
-
-        # 1) make randomized probas several times (E-step)
-        # 2) calculate the objective and pheromone update for each (pre-M-step)
-        for _ in range(n_probas):
-            # do the proba
-            proba_paths = _make_aco_proba(
-                n, n_salesmans,
-                m_evidence,
-                j_evidence,
-                greedy=False)
-
-            # calculate the objective
-            lengths = [_path_len(p, distances) for p in proba_paths]
-            max_length = max(lengths)
-            total_length = sum(lengths)
-            mean_length = total_length / len(lengths)
-
-            if objective == 'max':
-                cost = max_length
-            elif objective == 'total':
-                cost = total_length
-            else:
-                raise NotImplementedError(objective)
-
-            # commit to pheromone update buffer
-
-            upd = (1 / cost) / n_probas
-            m_upd = upd
-            j_upd = upd / len(proba_paths)
-            for k, pth1 in enumerate(proba_paths):
-                if lengths[k] == max_length and n_salesmans > 1:
-                    continue
-                for i in range(len(pth1)):
-                    c1, c2 = pth1[i-1], pth1[i]
-                    move_utility_update[c2][c1] += m_upd
-                    move_utility_update[c1][c2] += m_upd
-                for pth2 in proba_paths[k+1:]:
+        upd = (1 / cost) / self.n_probas
+        m_upd = upd
+        j_upd = upd
+        if USE_JUMP_PHEROMONE_DOWNSCALE:
+            j_upd /= len(paths)
+        for k, pth1 in enumerate(paths):
+            if JUMP_PHEROMONE == "heuristical":
+                for pth2 in paths[k + 1 :]:
                     for c1, c2 in itertools.product(pth1, pth2):
                         jump_utility_update[c1][c2] += j_upd
                         jump_utility_update[c2][c1] += j_upd
-                # jump_utility_update[prev_pth[-1]][pth1[0]] += upd
-                # jump_utility_update[pth1[0]][prev_pth[-1]] += upd
+            elif JUMP_PHEROMONE == "naive":
+                c1 = pth1[0]
+                c2 = paths[k - 1][-1]
+                jump_utility_update[c1][c2] += j_upd
+                jump_utility_update[c2][c1] += j_upd
+            else:
+                raise NotImplementedError
+            if lengths[k] == max_length and self.n_salesmans > 1:
+                if DECOUPLE_LONGEST_BY_JUMP:
+                    for c1, c2 in itertools.product(pth1, pth1):
+                        if c1 == c2:
+                            continue
+                        jump_utility_update[c1][c2] += j_upd
+                        jump_utility_update[c2][c1] += j_upd
+                if DECOUPLE_LONGEST_BY_MOVE:
+                    continue
+            for i in range(len(pth1)):
+                c1, c2 = pth1[i - 1], pth1[i]
+                move_utility_update[c2][c1] += m_upd
+                move_utility_update[c1][c2] += m_upd
+            # for pth2 in proba_paths[k+1:]:
+            #     for c1, c2 in itertools.product(pth1, pth2):
+            #         jump_utility_update[c1][c2] += j_upd
+            #         jump_utility_update[c2][c1] += j_upd
+            # jump_utility_update[prev_pth[-1]][pth1[0]] += upd
+            # jump_utility_update[pth1[0]][prev_pth[-1]] += upd
 
-            if _debug:
-                print('proba_paths =', proba_paths)
-        if _debug:
-            print('move_utility_update')
-            print(_format2d(move_utility_update))
-            print('jump_utility_update')
-            print(_format2d(jump_utility_update))
+        return cost
+
+    def update(self):
+        n = self.n
+        evaporation = self.evaporation
+        move_utility = self.move_utility
+        move_utility_update = self._move_utility_update
+        jump_utility = self.jump_utility
+        jump_utility_update = self._jump_utility_update
 
         # update pheromone and re-calculate transition probs (M-step)
         for i in range(n):
             for j in range(n):
                 if i == j:
                     continue
-                move_utility[i][j] = (
-                    (1-evaporation) * move_utility[i][j]
-                    + evaporation * move_utility_update[i][j]
-                )
-                jump_utility[i][j] = (
-                    (1-evaporation) * jump_utility[i][j]
-                    + evaporation * jump_utility_update[i][j]
-                )
-        if _debug:
-            print('move_utility')
-            print(_format2d(move_utility))
-            print('jump_utility')
-            print(_format2d(jump_utility))
+                move_utility[i][j] = (1 - evaporation) * move_utility[i][
+                    j
+                ] + evaporation * move_utility_update[i][j]
+                min_ju = min(map(min, jump_utility_update))
+                jump_utility[i][j] = (1 - evaporation) * jump_utility[i][
+                    j
+                ] + evaporation * (jump_utility_update[i][j] - min_ju)
 
-        m_evidence = [
-            [
-                move_heuristic[i][j] * (move_utility[i][j] ** herdness)
-                for j in range(n)
-            ]
-            for i in range(n)
-        ]
-        j_evidence = [
-            [
-                jump_heuristic[i][j] * (jump_utility[i][j] ** herdness)
-                for j in range(n)
-            ]
-            for i in range(n)
-        ]
+        self._update_evidences()
+        self._reset_update_buffers()
+
+    def get_move_probs(self, from_node, to_nodes, jumps_left):
+        move_utility = self._move_evidence
+        jump_utility = self._jump_evidence
+        moverow = [move_utility[from_node][i] for i in to_nodes]
+        jumprow = [jump_utility[from_node][i] for i in to_nodes]
+        if self.use_jump_balancing:
+            jump_prob = jumps_left / len(to_nodes)
+            move_prob = 1 - jump_prob
+        else:
+            jump_prob = 1 if jumps_left > 0 else 0
+            move_prob = 1 if jumps_left > len(to_nodes) else 0
+        return (move_prob, moverow), (jump_prob, jumprow)
+
+
+def solve_tsp(
+    distances,
+    heuristic: Heuristic,
+    callback=lambda paths, *utilities, **costs: None,
+    max_iteration=10,
+    n_salesmans=1,
+    objective="max",
+    _debug=False,
+):
+    if _debug > 0:
+        _debug -= 1
+    n = len(distances)
+    assert heuristic.n_salesmans < heuristic.n, (
+        "if there are no less salesmans than cities, "
+        "you do not need to force them to travel"
+    )
+    assert objective in ("max", "total")
+    assert JUMP_PHEROMONE in ("naive", "heuristical")
+
+    best_paths = _make_aco_proba(n, n_salesmans, heuristic, greedy=True)
+    best_cost = _get_proba_cost(best_paths, heuristic.distances)
+    callback(best_paths, heuristic.move_utility, L=best_cost)
+
+    for _ in range(max_iteration):
+        for _ in range(heuristic.n_probas):
+            proba_paths = _make_aco_proba(n, n_salesmans, heuristic, greedy=False)
+            cost = _get_proba_cost(proba_paths, heuristic.distances)
+            if cost < best_cost:
+                best_cost = cost
+                best_paths = proba_paths
+            heuristic.add_path(proba_paths, cost)
+
+        heuristic.update()
 
         # at each iteration we greedily compute current best solution
         # so callback can see the progress
-        best_paths = _make_aco_proba(
-            n, n_salesmans,
-            m_evidence,
-            j_evidence,
-            greedy=True)
-        callback(best_paths, move_utility, j_evidence)
+        greedy_paths = _make_aco_proba(n, n_salesmans, heuristic, greedy=True)
+        cost = _get_proba_cost(greedy_paths, heuristic.distances)
+        if cost < best_cost:
+            best_paths = greedy_paths
+            best_cost = cost
+        callback(greedy_paths, heuristic.move_utility, L=cost)
+
+    callback(best_paths, heuristic.move_utility, L=best_cost)
 
     return best_paths
 
 
+def _get_proba_cost(proba_paths, distances):
+    lengths = [_path_len(p, distances) for p in proba_paths]
+    return max(lengths)
+
+
 def _format1d(vector):
-    return '[' + ', '.join('{:.3f}'.format(v) for v in vector) + ']'
+    return "[" + ", ".join("{:.3f}".format(v) for v in vector) + "]"
 
 
 def _format2d(matrix):
-    return '\n'.join(map(_format1d, matrix))
+    return "\n".join(map(_format1d, matrix))
 
 
-def _make_aco_proba(n, n_salesmans, move_utility, jump_utility, greedy):
+def _make_aco_proba(n, n_salesmans, heuristic, greedy):
     not_visited = list(range(n))
     paths = [[0]]
     not_visited.remove(paths[0][0])
     jumps_left = n_salesmans - 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         pth = paths[-1]
         c = pth[-1]
-        moverow = [move_utility[c][i] for i in not_visited]
-        jumprow = [jump_utility[c][i] for i in not_visited]
-        jump_prob = jumps_left / len(not_visited)
-        move_prob = 1 - jump_prob
+        (move_prob, moverow), (jump_prob, jumprow) = heuristic.get_move_probs(
+            c, not_visited, jumps_left
+        )
         if greedy:
             jnc_p = _argmax(jumprow)
             mnc_p = _argmax(moverow)
-            jump = jump_prob*jumprow[jnc_p] > move_prob*moverow[mnc_p]
+            jump = jump_prob * jumprow[jnc_p] > move_prob * moverow[mnc_p]
         else:
             jnc_p = _weighted_choice(jumprow)
             mnc_p = _weighted_choice(moverow)
@@ -204,14 +236,12 @@ def _make_aco_proba(n, n_salesmans, move_utility, jump_utility, greedy):
             jump = _weighted_choice([mp, jp])
         nc_p = jnc_p if jump else mnc_p
         nc = not_visited.pop(nc_p)
-        del moverow[nc_p]
-        del jumprow[nc_p]
         if jump:
             paths.append([nc])
             jumps_left -= 1
         else:
             pth.append(nc)
-    assert len(paths) <= n_salesmans
+    assert len(paths) == n_salesmans
     return paths
 
 
@@ -242,22 +272,30 @@ def _argmax(weights):
 def _softmax(vector):
     exps = [math.exp(v) for v in vector]
     expsum = sum(exps)
-    return [e/expsum for e in exps]
+    return [e / expsum for e in exps]
 
 
-def _normalize(vector):
-    vsum = sum(vector)
-    return [v/vsum for v in vector]
+def _normalize(vector, norm=1.0):
+    factor = norm / sum(vector)
+    return [v * factor for v in vector]
+
+
+def _shift_to_positive(matrix):
+    minval = min(map(min, matrix))
+    return [[x - minval for x in row] for row in matrix]
 
 
 def _path_len(path, distances):
     d = 0
     for i in range(len(path)):
-        d += distances[path[i-1]][path[i]]
+        d += distances[path[i - 1]][path[i]]
     return d
 
 
 class GenerateVisualSvg:
+
+    _width = 500
+    _text_height = 50
 
     def __init__(self, savedir, coordinates):
         self.savedir = savedir
@@ -274,36 +312,49 @@ class GenerateVisualSvg:
 
         if os.path.exists(savedir):
             for fname in os.listdir(savedir):
-                if fname.endswith('.svg'):
+                if fname.endswith(".svg"):
                     fpath = os.path.join(savedir, fname)
                     os.remove(fpath)
         else:
             os.makedirs(savedir)
 
-    def __call__(self, paths, *utilities):
-        width = 500
+    def __call__(self, paths, *utilities, **costs):
+        width = self._width + 10
         contents = """
         <?xml version="1.0" encoding="utf-8" ?>
         <svg width="{}" height="{}" version="1.1"
              baseProfile="full" xmlns="http://www.w3.org/2000/svg">
-        """.lstrip().format(len(utilities) * width, width)
+        """.lstrip().format(
+            len(utilities) * width, width + self._text_height
+        )
+        contents += "<style>.txt {font: bold 26px sans-serif;}</style>"
+
+        costs_text = "; ".join(
+            "{}={:2f}".format(key, val) for key, val in costs.items()
+        )
+        paths_text = repr(paths)
+
+        contents += '<text x="25" y="25" class="txt">{} paths={}</text>'.format(
+            costs_text, paths_text
+        )
         for i, utility in enumerate(utilities):
             contents += self._render_graph(utility, paths, i * width)
             paths = []
         contents += "</svg>"
 
-        with open(os.path.join(self.savedir, '{}.svg'.format(self.i)), 'w') as f:
+        with open(os.path.join(self.savedir, "{}.svg".format(self.i)), "w") as f:
             f.write(contents)
         self.i += 1
 
     def _render_graph(self, edge_weights, highlighted_paths, x_offset):
-        contents = ''
+        contents = ""
         max_utility = max(map(max, edge_weights))
-        pathpairs = set(itertools.chain(*(
-            zip(p[:-1], p[1:])
-            for p in highlighted_paths
-        )))
+        pathpairs = set(
+            itertools.chain(*(zip(p[:-1], p[1:]) for p in highlighted_paths))
+        )
         pathpairs.update((p[-1], p[0]) for p in highlighted_paths)
+        w = self._width
+        th = self._text_height
 
         n = len(edge_weights)
         for i in range(n):
@@ -318,12 +369,12 @@ class GenerateVisualSvg:
                 line = """
                 <line x1="{}" y1="{}" x2="{}" y2="{}" stroke-width="{}" stroke="rgb({})"/>
                 """.format(
-                    x_offset + 500 * (self.coordinates[i][0] - self.xoffs) / self.xrange,
-                    500 * (self.coordinates[i][1] - self.yoffs) / self.yrange,
-                    x_offset + 500 * (self.coordinates[j][0] - self.xoffs) / self.xrange,
-                    500 * (self.coordinates[j][1] - self.yoffs) / self.yrange,
+                    x_offset + w * (self.coordinates[i][0] - self.xoffs) / self.xrange,
+                    th + w * (self.coordinates[i][1] - self.yoffs) / self.yrange,
+                    x_offset + w * (self.coordinates[j][0] - self.xoffs) / self.xrange,
+                    th + w * (self.coordinates[j][1] - self.yoffs) / self.yrange,
                     max(minw, 3 * edge_weights[i][j] / max_utility),
-                    ','.join(map(str, color)),
+                    ",".join(map(str, color)),
                 )
                 contents += line
         return contents
